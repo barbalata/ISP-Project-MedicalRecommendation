7b44cd26966b1b59d6eed9372f33f3f655f779e8
HuleaVlad <hulea.vlad@yahoo.ro>
2018-05-05 17:33:22 +0300

    Doctor 2

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import jdk.nashorn.internal.runtime.Specialization;
 
 import java.util.ArrayList;
@@ -11,28 +11,73 @@ public class Doctor {
 
     String name;
     public ArrayList<SpecializationEnum> Specialization;
-    Map<Date,ArrayList<Apppointment>> Disponibility;
+    Map<Date, ArrayList<Appointment>> Disponibility;
     String location;
+    Date date;
     public ArrayList<Drug> Drug;
 
-    public Doctor(String name, ArrayList<SpecializationEnum> Specialization, String location){
-        this.name=name;
-        this.Specialization= Specialization;
-        this.location= location;
+    public Doctor(String name, ArrayList<SpecializationEnum> Specialization, String location) {
+        this.name = name;
+        this.Specialization = Specialization;
+        this.location = location;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public void SetName(String name) {
+        this.name = name;
     }
 
-    public String getName(){
+    public String GetName() {
         return name;
     }
 
-    public void setSpecialization(ArrayList<SpecializationEnum> Specialization){
-        this.Specialization=Specialization;
+    public void SetSpecialization(ArrayList<SpecializationEnum> Specialization) {
+        this.Specialization = Specialization;
     }
-=======
-public class Doctor {
->>>>>>> 23c1f47d859e0bdf9af941dff39f2d646f6684eb
+
+    public ArrayList<SpecializationEnum> GetSpecialization(){
+        return Specialization;
+    }
+
+    public void SetAppointment (Date date){
+        this.date=date;
+    }
+
+    public Date GetAppointment(){
+        return date;
+    }
+
+    public void SetLocation(String location){
+        this.location=location;
+    }
+
+    public String GetLocation(){
+        return location;
+    }
+
+    public ArrayList<Appointment> CheckAppointment(Date startDate,Date endDate){
+        ArrayList<Appointment> Appointments = new ArrayList<Appointment>();
+        for(Date item:Disponibility.keySet()){
+            if(item.after(startDate) && item.before(endDate)){
+                for(Appointment appointment:Disponibility.get(item)){
+                    Appointments.add(appointment);
+                }
+            }
+        }
+        return Appointments;
+    }
+
+   
+
+    public void ShowDoctor(){
+        System.out.println(this.name);
+        System.out.println(this.location);
+        for(SpecializationEnum specialization:Specialization){
+            System.out.println(specialization.toString());
+        }
+    }
+
+
+
+
 }
+
