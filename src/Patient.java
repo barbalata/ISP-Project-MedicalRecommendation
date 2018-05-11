4bd360439dc6c2a0a472a424371cdcb300156b1a
Cătălin-Răzvan Barbălată <barbalata.razvan@gmail.com>
2018-05-11 04:17:44 +0300

    Rename Patient's Method

--- src/Patient.java
+++ src/Patient.java
@@ -8,67 +8,68 @@ public class Patient {
     private ArrayList<AllergenEnum> Allergens;
     private ArrayList<AffectionEnum> Affections;
 
-    public Patient(){
+    public Patient() {
         this.Allergens = new ArrayList<>();
         this.Affections = new ArrayList<>();
     }
 
-    public Patient(String name, GenderEnum gender, int age){
+    public Patient(String name, GenderEnum gender, int age) {
         this.Age = age;
         this.Name = name;
         this.Gender = gender;
     }
 
     //region Gets and Sets
-    public void SetName(String name){
+    public void SetName(String name) {
         this.Name = name;
     }
-    public void SetGender(GenderEnum gender){
-        this.Gender = gender;
-    }
 
-    public void SetAge(int age){
-        this.Age = age;
-    }
-
-    public void SetAllergens(ArrayList<AllergenEnum> allergens){
-        this.Allergens.addAll(allergens);
-    }
-
-    public void SetAllergen(AllergenEnum allergen){
-        this.Allergens.add(allergen);
-    }
-
-    public void SetAffections (ArrayList<AffectionEnum> affections){
-        this.Affections.addAll(affections);
-    }
-
-    public void SetAffection (AffectionEnum affection){
-        this.Affections.add(affection);
-    }
-
-    public String GetName(){
+    public String GetName() {
         return this.Name;
     }
 
-    public GenderEnum GetGender(){
+    public void SetGender(GenderEnum gender) {
+        this.Gender = gender;
+    }
+
+    public GenderEnum GetGender() {
         return this.Gender;
     }
 
-    public int GetAge(){
+    public void SetAge(int age) {
+        this.Age = age;
+    }
+
+    public int GetAge() {
         return this.Age;
     }
 
-    public ArrayList<AllergenEnum> GetAllergens(){
+    public void SetAllergens(ArrayList<AllergenEnum> allergens) {
+        this.Allergens.addAll(allergens);
+    }
+
+    public void SetAllergen(AllergenEnum allergen) {
+        this.Allergens.add(allergen);
+    }
+
+    public void SetAffections(ArrayList<AffectionEnum> affections) {
+        this.Affections.addAll(affections);
+    }
+
+    public void SetAffection(AffectionEnum affection) {
+        this.Affections.add(affection);
+    }
+
+    public ArrayList<AllergenEnum> GetAllergens() {
         return Allergens;
     }
 
-    public ArrayList<AffectionEnum> GetAffections(){
+    public ArrayList<AffectionEnum> GetAffections() {
         return Affections;
     }
     //endregion
 
-    public void ShowPatient(){
+    public void ShowPatient() {
         System.out.println("The information about patient:");
         System.out.println("\tName: " + this.Name + ";");
 
@@ -89,17 +90,17 @@ public class Patient {
         System.out.print("\n");
     }
 
-    public ArrayList<Drug> SearchDrug(Doctor doctor){
+    public ArrayList<Drug> SearchDrug(Doctor doctor) {
         return doctor.SearchDrug(this.Affections, this.Allergens, this.Age);
     }
 
-    public ArrayList<Drug> SearchDrug(Doctor doctor, ArrayList<AffectionEnum> affections){
+    public ArrayList<Drug> SearchDrug(Doctor doctor, ArrayList<AffectionEnum> affections) {
         return doctor.SearchDrug(affections, this.Allergens, this.Age);
     }
 
-    public boolean SetAppointment(Date date, Doctor doctor){
-        if(!doctor.CheckAppointment(date)){
-            doctor.SetAppointment(date, this);
+    public boolean SetAppointment(Date date, LocationEnume location, Doctor doctor) {
+        if (!doctor.CheckAppointment(date, location)) {
+            doctor.SetAppointment(date, location, this);
             return true;
         }
         return false;
